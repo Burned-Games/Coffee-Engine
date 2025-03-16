3c3218e6c73a74d8c25075d52ea6d4fd654c2c44
Hugo Planell <hugoplanellmoreno@gmail.com>
2025-03-16 13:17:40 +0100

    Update Components.h

--- CoffeeEngine/src/CoffeeEngine/Scene/Components.h
+++ CoffeeEngine/src/CoffeeEngine/Scene/Components.h
@@ -439,6 +439,7 @@
          float Intensity = 1.0f; ///< The intensity of the light.
  
          float Angle = 45.0f; ///< The angle of the light.
+         float cutoff = glm::cos(glm::radians(Angle)); ///< The cutoff of the light.
  
          int type = static_cast<int>(Type::DirectionalLight); ///< The type of the light.
  
