cbb0478896fafeaaa8fd6eeac3458f30bfde58c1
Hugo Planell <hugoplanellmoreno@gmail.com>
2025-05-06 23:32:26 +0200

    Added a button to set a material as embedded in the scene

--- CoffeeEditor/Panels/SceneTreePanel.cpp
+++ CoffeeEditor/Panels/SceneTreePanel.cpp
@@ -1186,6 +1186,19 @@ namespace Coffee
                         entity.RemoveComponent<MaterialComponent>();
                     }
                 }
+                if(materialComponent.material and ImGui::TreeNode("Resource"))
+                {
+                    bool embedded = materialComponent.material->IsEmbedded();
+                    ImGui::Checkbox("Embedded", &embedded);
+                    if (embedded != materialComponent.material->IsEmbedded())
+                    {
+                        materialComponent.material->SetEmbedded(embedded);
+                    }
+                    ImGui::Text("Path: %s", materialComponent.material->GetPath().string().c_str());
+                    ImGui::Text("UUID: %s", std::to_string(materialComponent.material->GetUUID()).c_str());
+
+                    ImGui::TreePop();
+                }
             }
 
             if (ShowShaderQuickLoad)
